281a92ad03f44f374f8cf1f5758b997ce8da5fda
Anya <anya.s.litvinova@gmail.com>
2020-01-21 15:23:22 -0500

    some semantic styling

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,31 +1,46 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import SingleMapContainer from "./SingleMapContainer";
+import { Icon, List, Button } from "semantic-ui-react";
+
 
 class Profile extends Component {
-  render() {
-    console.log(this.props.currentUser);
-    return (
-      <div>
-        {this.props.currentUser ? (
-          <div>
-            <h1>Welcome to Your Profile, {this.props.currentUser.email}!</h1>
-            <h2>Here are your fav restuarants:</h2>
-            <ul>
-              {this.props.currentUser.favorites !== undefined
-                ? this.props.currentUser.favorites.map(restaurant => (
-                    <li key={restaurant.id}>{restaurant.name}</li>
-                  ))
-                : null}
-            </ul>
-            <SingleMapContainer />
+
+  handleClick = (e) => {
+  console.log("I've been clicked!");
+}
+
+render() {
+  console.log(this.props.currentUser);
+  return (
+    <div>
+      {this.props.currentUser ? (
+        <div>
+          <div className="ui container">
+              <h1>Hello, {this.props.currentUser.name}!</h1>
+              <div className="ui segment">
+                <h3>Favorites</h3>
+                <List divided verticalAlign='middle' size="huge">
+                  {this.props.currentUser.favorites !== undefined ? (
+                    this.props.currentUser.favorites.map(restaurant => (
+                      <List.Item key={restaurant.id}>
+                        <List.Content>
+                          <List.Header>{restaurant.name} <Button circular icon="close" onClick={this.handleClick}></Button></List.Header>
+                        </List.Content>
+                      </List.Item>
+                    )
+                  )) : (<p>Loading</p>)
+                }
+              </List>
+              </div>
           </div>
-        ) : (
-          <p>Loading</p>
-        )}
+		  <SingleMapContainer />
       </div>
-    );
-  }
+	  ) : (<p>Loading</p>)
+          }
+	</div>
+	)
+}
 }
 
 const mapStateToProps = state => ({
